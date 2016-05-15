0d1cc1fbda76aad91832accfa7598f6c6092e735
David Storm <dadibom@hotmail.com>
2016-05-15 15:23:12 +0200

    Updated the client to work with the server

--- public/js/manochat.js
+++ public/js/manochat.js
@@ -5,24 +5,51 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 	ws = new WebSocket("ws://"+window.location.hostname+":8080");
 	ws.onopen = function (event) {
-	  	ws.send(JSON.stringify({
-	  		action:"sub",
-	  		channel:"global"
-	  	})); 
 	  	ws.onmessage = function (event) {
+	  		console.log(event);
 	  		var obj = JSON.parse(event.data);
-		  	con.innerHTML = con.innerHTML+"<span class='channel'>#"+obj.channel+"</span> "+obj.message+"<br>";
+
+	  		switch(obj.action){
+	  			case 'broadcast':
+				  	con.innerHTML = con.innerHTML+"<span class='channel'>#"+obj.channel+"</span> "+obj.message+"<br>";
+				  	break;
+				case 'auth':
+					if(obj.accepted){
+						document.getElementById("popup-blocker").classList.add("hidden");
+						send({
+					  		action:"sub",
+					  		channel:"global"
+					  	}); 
+					}
+					break;
+				case 'suggest-username':
+					document.getElementById("username-input").value = obj.username;
+					break;
+		}
 		};
 	};
 	document.getElementById("input").onsubmit = function(){
-		var m = JSON.stringify({
+		send({
 	  		action:"shout",
 	  		channel:"global",
 	  		message:inp.value
-	  	});
-		ws.send(m); 
-	  	//console.log(m);
+	  	}); 
 		inp.value = "";
 		return false;
 	};
 });
+
+function send(obj){
+	//TODO if socket is not yet connected
+	ws.send(JSON.stringify(obj));
+}
+
+function auth(){
+	var field = document.getElementById("username-input");
+	send({
+		action:'auth',
+		username:field.value
+	});
+	field.value = "";
+	return false;
+}
